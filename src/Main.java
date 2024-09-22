1d3ed9bb1b8254c09f5d3ead7061a22810712c61
Timothy Fee <keys333@proton.me>
2024-09-22 18:53:51 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+   public static void main(String[] args) {
+
+    int testNum = 78;
+
+    if (testNum % 2 == 0) {
+      System.out.println("Even");
+    }
+    
+    else {
+      System.out.println("Odd");
+    }
+  }
+}
